4111cecb2c1939736663d91ece38d92fc81e9d4d
Xavi2311 <xaviarjona23@gmail.com>
2021-11-23 08:44:20 +0100

    Update script.js

---
+++ Altres/script.js
@@ -0,0 +1,6 @@
+var numero = 1234;
+var bigint = 5678;
+console.log((bigint+numero).toFixed(1));
+console.log((bigint-numero).toFixed(1));
+console.log((bigint*numero).toFixed(1));
+console.log((bigint/numero).toFixed(1));
