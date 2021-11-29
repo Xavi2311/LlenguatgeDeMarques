1d46b102f16af69902e9b56a83a3e43851f6a444
Xavi2311 <xaviarjona23@gmail.com>
2021-11-29 09:48:33 +0100

    Create ex8.js

---
+++ JSFuncions Xavi Arjona/ex8/ex8.js
@@ -0,0 +1,10 @@
+function ex8(num1,num2){
+    if (num1+num2==30){
+        console.log(true)
+    }
+    else if (num1==30||num2==30){
+        console.log(true) }
+        
+    else console.log(false)
+}
+ex8 (14,16);
