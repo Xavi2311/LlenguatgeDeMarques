343ab48616f99dc58b6d610bd61615b252abcecb
Xavi2311 <xaviarjona23@gmail.com>
2021-11-29 23:29:28 +0100

    Create ex9.js

---
+++ JSFuncions Xavi Arjona/ex9/ex9.js
@@ -0,0 +1,10 @@
+ var ex9 = function(num1,num2){
+    if (num1+num2==30){
+        console.log(true)
+    }
+    else if (num1==30||num2==30){
+        console.log(true) }
+        
+    else console.log(false)
+}
+ex9 (14,16);
