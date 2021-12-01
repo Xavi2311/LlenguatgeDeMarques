71f3c9ab42987d159688e77ad0b06971ef543333
Xavi2311 <xaviarjona23@gmail.com>
2021-12-01 23:39:58 +0100

    Create ex9.js

---
+++ JSLogica Xavi Arjona/ex9.js
@@ -0,0 +1,10 @@
+var cinc =5;
+var tres =3;
+var multiple=1;
+var total=0;
+do { 
+    total=total+(cinc*multiple);
+    total=total+(tres*multiple);
+    multiple++
+} while (total<1000);
+console.log(total)
