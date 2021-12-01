a38a9bfc946eadbf1780693961d012008414c970
Xavi2311 <xaviarjona23@gmail.com>
2021-12-01 11:40:48 +0100

    Create ex2.js

---
+++ JSLogica Xavi Arjona/ex2.js
@@ -0,0 +1,7 @@
+var x =2;
+var y=11;
+x >= 10 && x<=50 ?  console.log("El numero esta dintre del rang")
+                :  console.log("El numero no es a dintre del rang");
+y >= 10 && y<=50 ?  console.log("El numero esta dintre del rang")
+                :  console.log("El numero no es a dintre del rang");
+   
