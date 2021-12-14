938a749716f14b165a482d816b6ae4fc3bfcde47
Xavi2311 <xaviarjona23@gmail.com>
2021-12-14 22:44:33 +0100

    Create js7.js

---
+++ JSDOM Xavi Arjona/js7.js
@@ -0,0 +1,7 @@
+export function addImage(){
+    var img = document.createElement("img");
+    img.src =
+    "link";
+    var src = document.getElementById("background");
+    src.appendChild(img;)
+}
